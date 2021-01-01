a5d45f6e1080b853b85a945670861681a085e222
CAPCHA519 <ckpow9@gmail.com>
2021-01-01 18:14:29 +0900

    기본적인 데이터 타입 공부

---
+++ practice language/java/PrimitiveDataTypeExam.java
@@ -0,0 +1,20 @@
+public class PrimitiveDataTypeExam
+{
+    public static void main(String[] args){
+     boolean isFun = true;
+     System.out.println(isFun);
+
+     char c = 'f'; //문자 하나를 표현가능 여러개 불가
+
+     int x = 59;
+
+     long bing = 334234235l;  //롱 타입은 뒤에 l 사용
+
+     float f = 32.4f;
+
+     double d = 3424234.5;
+
+
+
+    }
+}
