eeead8e8394a9121f58e13a0eda506f953c15881
CAPCHA519 <ckpow9@gmail.com>
2021-01-01 17:45:07 +0900

    변수 실습

---
+++ practice language/java/valuableexam.java
@@ -0,0 +1,16 @@
+public class valuableexam {
+
+    public static void main(String[] args) {
+        int count;
+
+        count = 10;
+        count = 20;
+
+        System.out.println(count);
+
+        double avg = 11.1;
+
+        String name = "carrat";
+
+    }
+}
