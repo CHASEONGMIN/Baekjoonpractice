7c2341cf60603c2bd2efc2ec76b31d6b7b586bcf
CAPCHA519 <ckpow9@gmail.com>
2021-01-13 16:16:22 +0900

    210113 파이썬 기본 실습

---
+++ python/ditionary.py
@@ -0,0 +1,9 @@
+phone_book = {'파파존스': '1234-5678'}
+print(phone_book['파파존스'])
+
+
+#기본 터미널 쉘 바꾸기 - Ctrl Shift P
+#default Shell
+#Git bash 선택
+#Terminal 죽이기 > 다시 키기
+#Ctrl L - 터미널 클리어
---
+++ python/dust.py
@@ -0,0 +1,3 @@
+#dictionary = {key:value}
+dust = {'영등포구':58, '강남구':40, '서초구':60}
+print(dust['영등포구'])
